69daf3b4542ee9d6324fd51a025480a899f79435
sushruthajirnis <sushrut.hajirnis@gmail.com>
2012-02-13 22:13:41 -0800

    first commit

---
+++ myproj/src/myprojt/UPanddown.java
@@ -0,0 +1,16 @@
+package myprojt;
+
+public class UPanddown {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+	
+	
+	}
+
+}
